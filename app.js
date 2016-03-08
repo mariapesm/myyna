c889d69ba88062ccd1f401f906dfe7003e5bcf76
jmtt89 <jmtt89@gmail.com>
2016-03-07 23:57:15 -0430

    update npm libraries

--- app.js
+++ app.js
@@ -35,43 +35,62 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var exphbs  = require('express3-handlebars');
+var exphbs  = require('express-handlebars');
 var helmet  = require('helmet');
 var fs = require('fs');
 var hbs = require('handlebars');
 var engines = require('consolidate');
-var io = require('socket.io');
 global.app = express();
-
 global.sleekConfig = {};
-require(path.join(__dirname,'application/config/config.js'));
-app.configure(function(){
-    app.set('env', sleekConfig.env);
-    // all environments
-    app.set('port', process.env.PORT || sleekConfig.appPort);
-    app.set('host', sleekConfig.appHost ? sleekConfig.appHost : 'localhost');
-    app.set('views', path.join(__dirname, 'application/views'));
-    app.set('view engine', 'handlebars');
-    app.engine('html',  exphbs({defaultLayout: 'default',
-                                layoutsDir: path.join(__dirname, 'application/layouts/'), extname:".html"})
-                ); 
-    app.use(express.favicon(path.join(__dirname, 'public/favicon.ico'))); 
-    app.use(express.logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded());  
-    app.use(helmet.xframe());
-    app.use(helmet.iexss());
-    app.use(helmet.contentTypeOptions());
-    app.use(helmet.cacheControl());
-    app.use(express.methodOverride());
-    app.use(express.cookieParser('CubEtNoDeSlEek'));
-    app.use(express.session());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(path.join(__dirname, 'uploads')));
-    app.set('strict routing');
 
-    
-});
+
+require(path.join(__dirname,'application/config/config.js'));
+
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({ dest: './uploads' });
+var cookieParser = require('cookie-parser');
+var static = require('serve-static')
+var errorHandler = require('errorhandler');
+
+var app = express();
+
+app.set('env', sleekConfig.env || process.env.NODE_ENV);
+
+// all environments
+app.set('port', process.env.PORT || sleekConfig.appPort);
+app.set('host', sleekConfig.appHost ? sleekConfig.appHost : 'localhost');
+app.set('views', path.join(__dirname, 'application/views'));
+app.set('view engine', 'handlebars');
+app.engine('html',  exphbs({
+        defaultLayout: 'default',
+        layoutsDir: path.join(__dirname, 'application/layouts/'),
+        extname:".html"
+    })
+); 
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(static(path.join(__dirname, 'public')));
+app.use(static(path.join(__dirname, 'uploads')));
+app.use(methodOverride());
+app.use(cookieParser('CubEtNoDeSlEek'))
+app.use(session({ resave: true,
+                  saveUninitialized: true,
+                  secret: 'CubEtNoDeSlEek' }));
+
+app.use(helmet());
+
+app.set('strict routing');
+
+
 
 //set Site url
 //global.sleekConfig.siteUrl = app.get('host')+':'+app.get('port');
@@ -88,7 +107,7 @@ fs.exists(sFolderPath, function(exists) {
 });
 // development only
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+    app.use(errorHandler());
 } else {
     //prevent crash
     process.on('uncaughtException', function (exception) {
@@ -96,12 +115,13 @@ if ('development' == app.get('env')) {
     });
 }
 
-var server = http.createServer(app);
+
 try {
-    global.sio = io.listen(server, {log: false});
+    var server = http.createServer(app);
+    global.sio = require('socket.io')(server);
     server.listen(app.get('port'), function(){
-      console.log('application running at ' + sleekConfig.siteUrl);
+        console.log('application running at ' + sleekConfig.siteUrl);
     });
 } catch (e) {
-    system.log(e);
+    console.error(e);
 }
